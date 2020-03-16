32e0694d6b0a6ce88bea7980a5d90e69aabe0d30
Sharp <iveoname@gmail.com>
2020-03-16 11:01:01 +0800

    feat: inject normalize.scss to the whole project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { SFC } from 'react';
 import { Switch } from 'react-router-dom';
 import { NestedRoute, routes } from '@router/index';
+import '@scss/normalize.scss';
 
 const App: SFC<{}> = () => {
   return (
