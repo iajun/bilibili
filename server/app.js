471532a9db91f56148d030b07eb776e988ebfff2
Sharp <iveoname@gmail.com>
2020-03-25 23:00:50 +0800

    style: remove redundant code and files

--- server/app.js
+++ server/app.js
@@ -2,12 +2,11 @@
  * @Date: 2020-03-14 20:50:20
  * @Author: Sharp
  * @LastEditors: Sharp
- * @LastEditTime: 2020-03-24 16:11:09
+ * @LastEditTime: 2020-03-25 23:00:43
  */
 const express = require('express');
 const setAppEnv = require('./setAppEnv');
 const request = require('./util/request');
-const fs = require('fs');
 
 const LISTEN_PORT = 3020;
 const app = express();
